035809c05ec4fea39ad78fe57fa4ad81481e164b
NotANewt <meegafauna@gmail.com>
2022-09-02 14:00:55 -0500

    working on bringing in components that will not change with routing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+require("bootstrap/dist/css/bootstrap.min.css");
+require("bootstrap/bootstrap.bundle");
+
+import { PLATFORM } from "aurelia-pal";
+
 export class App {
-  message = 'Hello World!';
+  configureRouter(config, router) {
+    config.title = "Aurelia";
+
+    config.map([
+      {
+        route: ["", "home"],
+        name: "home",
+        moduleId: PLATFORM.moduleName("./components/home/home"),
+        nav: true,
+        title: "Home",
+      },
+      {
+        route: "about",
+        name: "about",
+        moduleId: PLATFORM.moduleName("./components/about/about"),
+        nav: true,
+        title: "About",
+      },
+    ]);
+
+    this.router = router;
+  }
 }
